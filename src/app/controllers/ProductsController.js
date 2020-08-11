01141d7b1bb277f37c7b3621e92b144977dbf874
Jacó Apolinário <jacoapollinario@gmail.com>
2020-08-11 11:35:13 -0300

    Launchstore

---
+++ src/app/controllers/ProductsController.js
@@ -0,0 +1,50 @@
+const { formatPrice } = require('../../lib/utils')
+
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+
+module.exports = {
+    create(req, res) {
+        //Pegar Categorias
+        Category.all()
+        .then(function(results) {
+
+            const categories = results.rows
+
+            return res.render("products/create.njk", { categories })
+
+        }).catch(function(err) {
+            throw new Error(err)
+        })
+        
+    },
+    async post(req, res) {
+        //Logica de Salvar
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fields!')
+            }
+        }
+
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        return res.redirect(`/product/${productId}`)        
+    },
+    async edit(req, res) {
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if (!product) return res.send("product not found")
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+
+        results = await Category.all()
+        const categories = results.rows
+
+        return res.render("products/edit.njk", { product, categories })
+    }
+}
---
+++ src/routes.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const routes = express.Router()
+const ProductController = require('./app/controllers/ProductsController')
+
+routes.get('/', function(req, res) {
+    return res.render("layout.njk")
+})
+
+routes.get('/products/create', ProductController.create)
+routes.get('/products/:id/edit', ProductController.edit)
+routes.post('/products', ProductController.post)
+
+// Alias
+routes.get('/ads/create', function(req, res) {
+    return res.redirect("/products/create")
+})
+
+module.exports = routes
