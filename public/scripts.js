b71a45fa6ab580bb9cdb434e559b77b2f41fe20e
Jacó Apolinário <jacoapollinario@gmail.com>
2020-10-12 19:58:32 -0300

    News features

--- public/scripts.js
+++ public/scripts.js
@@ -5,7 +5,7 @@ const Mask = {
         }, 1)
     },
     formatBRL(value) {
-        value = value.replace(/\D/g,"")
+        value = value.replace(/\D/g, "")
 
         return new Intl.NumberFormat('pt-BR', {
             style: 'currency',
@@ -13,6 +13,7 @@ const Mask = {
         }).format(value/100)
     }
 }
+
 
 const PhotosUpload = {
     input: "",
@@ -118,5 +119,35 @@ const PhotosUpload = {
     }
 }
 
+const ImageGallery = {
+    highlight: document.querySelector('.gallery .highlight > img'),
+    previews: document.querySelectorAll('.gallery-preview img'),
+    
+    setImage(e) {
+        const { target } = e
 
+        ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+        target.classList.add('active')
 
+        ImageGallery.highlight.src = target.src
+        Lightbox.image.src = target.src
+    }
+}
+
+const Lightbox = {
+    target: document.querySelector('.lightbox-target'),
+    image: document.querySelector('.lightbox-target img'),
+    closeButton: document.querySelector('.lightbox-target a.lightbox-close'),
+    open() {
+        Lightbox.target.style.opacity = 1
+        Lightbox.target.style.top = 0
+        Lightbox.target.style.bottom = 0
+        Lightbox.closeButton.style.top = 0
+    },
+    close() {
+        Lightbox.target.style.opacity = 0
+        Lightbox.target.style.top = "-100%"
+        Lightbox.target.style.bottom = "initial"
+        Lightbox.closeButton.style.top = "-80px"
+    }
+}
