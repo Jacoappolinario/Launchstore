73088671bd98a3fd53fcabaf32d3e5565b369c99
Jacó Apolinário <jacoapollinario@gmail.com>
2020-10-13 12:09:23 -0300

    add correction script

--- public/scripts.js
+++ public/scripts.js
@@ -5,7 +5,7 @@ const Mask = {
         }, 1)
     },
     formatBRL(value) {
-        value = value.replace(/\D/g, "")
+        value = value.replace(/\D/g,"")
 
         return new Intl.NumberFormat('pt-BR', {
             style: 'currency',
