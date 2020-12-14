8b9016a4e6b893f63222b27dde271b3703cc6dfb
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-14 10:33:20 -0300

    news features

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const routes = express.Router()
 const multer = require('./app/middlewares/multer')
+
 const ProductController = require('./app/controllers/ProductsController')
 const ProductsController = require('./app/controllers/ProductsController')
+const HomeController = require('./app/controllers/HomeController')
 
-routes.get('/', function(req, res) {
-    return res.render("layout.njk")
-})
+routes.get('/', HomeController.index)
 
 routes.get('/products/create', ProductController.create)
 routes.get('/products/:id', ProductsController.show)
