06b791c721c354379d426377a728c85e9c2c15df
Jacó Apolinário <jacoapollinario@gmail.com>
2020-12-22 11:00:34 -0300

    search logical

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -82,7 +82,7 @@ module.exports = {
 
         if (category) {
             filterQuery = `${filterQuery}
-            products.category_id = ${category_id}
+            products.category_id = ${category}
             AND
             `
         }
@@ -94,12 +94,11 @@ module.exports = {
         `
 
         query = `
-            SELECT products.* 
+            SELECT products.*, 
                 categories.name AS category_name
             FROM products
             LEFT JOIN categories ON (categories.id = products.category_id)
             ${filterQuery}
-            GROUP BY categories.name
         `
 
         return db.query(query)
