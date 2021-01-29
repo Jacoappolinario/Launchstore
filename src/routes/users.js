da9ca3710dd9c972e287b60fb3fb980b57ef0592
Jacó Apolinário <jacoapollinario@gmail.com>
2021-01-29 12:35:10 -0300

    new features

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,9 +7,9 @@ const UserController = require('../app/controllers/UserController')
 const Validator = require('../app/validators/user')
 
 // //Login/Logout
-// routes.get('/login', SessionController.loginForm)
+routes.get('/login', SessionController.loginForm)
 // routes.post('/login', SessionController.login)
-// routes.post('/logout', SessionController.logout)
+routes.post('/logout', SessionController.logout)
 
 // //Reset password/forgot
 // routes.get('/forgot-password', SessionController.forgotForm)
@@ -22,7 +22,7 @@ routes.get('/register', UserController.registerForm)
 routes.post('/register', Validator.post, UserController.post)
 
 routes.get('/', Validator.show, UserController.show)
-routes.put('/', UserController.update)
+routes.put('/', Validator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
 module.exports = routes
