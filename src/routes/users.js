c055f762fe01968858109f31e4d787579003706d
Jacó Apolinário <jacoapollinario@gmail.com>
2021-02-01 11:30:20 -0300

    add session users

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,11 +4,14 @@ const routes = express.Router()
 const SessionController = require('../app/controllers/SessionController')
 const UserController = require('../app/controllers/UserController')
 
-const Validator = require('../app/validators/user')
+const UserValidator = require('../app/validators/user')
+const SessionValidator = require('../app/validators/session')
+
+const { isLoggedRedirectToUsers, onlyUsers } = require('../app/middlewares/session')
 
 // //Login/Logout
-routes.get('/login', SessionController.loginForm)
-// routes.post('/login', SessionController.login)
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
+routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // //Reset password/forgot
@@ -19,10 +22,10 @@ routes.post('/logout', SessionController.logout)
 
 // //User register
 routes.get('/register', UserController.registerForm)
-routes.post('/register', Validator.post, UserController.post)
+routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', Validator.show, UserController.show)
-routes.put('/', Validator.update, UserController.update)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
+routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
 module.exports = routes
