6b32f3981e0a7fe6a7f45166dca3cefed01a699a
Jacó Apolinário <jacoapollinario@gmail.com>
2021-02-05 12:21:26 -0300

    add reset password

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,5 +1,7 @@
-const crypto = require('crypto')
 const User = require('../models/User')
+
+const { hash } = require('bcryptjs')
+const crypto = require('crypto')
 const mailer = require('../../lib/mailer')
 
 module.exports = {
@@ -41,7 +43,7 @@ module.exports = {
                 html: `<h2>Perdeu a senha?</h2>
                 <p>Não se preocupe, clique no link abaixo para recuperar sua senha</p>
                 <p>
-                    <a href="https://localhost:3000/users/password-reset?token=${token}" target="_blank">
+                    <a href="http://localhost:3000/users/password-reset?token=${token}" target="_blank">
                         RECUPERAR SENHA
                     </a>
                 </p>
@@ -59,5 +61,39 @@ module.exports = {
             })
         }
 
+    },
+    resetForm(req, res) {
+        return res.render("session/password-reset", { token: req.query.token })
+    },
+    async reset(req, res) {
+        const user = req.user
+
+        const { password, token } = req.body
+
+        try {
+            // cria um novo hash de senha
+            const newPassword = await hash(password, 8)
+            
+            // atualiza o usuário
+            await User.update(user.id, {
+                password: newPassword,
+                reset_token: "",
+                reset_token_expires: ""
+            })
+            
+            // avisar o usuário que ele tem uma nova senha
+            return res.render("session/login", {
+                user: req.body,
+                success: "Senha atualizada! Faça o seu login"
+            })
+
+        } catch(err) {
+            console.error(err)
+            return res.render("session/password-reset", {
+                user: req.body,
+                token,
+                error: "Erro inesperado, tente novamente!"
+            })
+        }
     }
 }
